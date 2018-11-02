a0dd198e7cabad475695ffacbdd142de8dd5b6f7
huiyifyj <jxfengyijie@gmail.com>
2018-11-02 13:55:38 +0800

    Remove database.js to database folder

---
+++ database/database.js
@@ -0,0 +1,26 @@
+import mysql from 'mysql';
+
+import databaseConfig from './config/database.json';
+
+const pool = mysql.createPool(databaseConfig);
+
+let query = ( sql, values ) => {
+
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err)
+            } else {
+                connection.query(sql, values, (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                    connection.release();
+                });
+            }
+        });
+    });
+
+}
