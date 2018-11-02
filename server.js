35ec00e741e4e1422a0878bc58560f2bb4a19d2c
huiyifyj <jxfengyijie@gmail.com>
2018-11-02 18:43:23 +0800

    Replace koa-static with koa-static-cache

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import path from 'path';
 
 import Koa from 'koa';
-import server from 'koa-static';
 import logger from 'koa-logger';
+import staticCache from 'koa-static-cache';
 import bodyParser from 'koa-bodyparser';
 import render from 'koa-art-template';
 
@@ -19,7 +19,9 @@ render(app, {
 
 app.use(logger())
    .use(bodyParser())
-   .use(server(path.join(__dirname, '/public')))
+   .use(staticCache(path.join(__dirname, "./public"), {
+        maxAge: 365 * 24 * 60 * 60
+   }))
    .use(routers.routes())
    .use(routers.allowedMethods())
    .listen(3000, () => console.log('Listening on port 3000'));
